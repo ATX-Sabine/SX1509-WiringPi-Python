6e8fb2f0ab67de5dde5d6dc03258c89a3a6a3ab2
Kas Perch <the@nodebotani.st>
2017-12-20 11:48:37 -0600

    Starting scaffolding

--- SX1509.py
+++ SX1509.py
@@ -1,3 +1,23 @@
 class SX1509:
-  def __init__(self):
-    self.test = True
+
+  REGISTERS = {
+    'CHECK': 0x13
+  }
+
+  PIN_MODE = {
+    'OUTPUT': 0,
+    'INPUT': 1,
+    'PWM': 3
+  }
+
+  PIN_STATE = {
+    'LOW': 0,
+    'HIGH': 1
+  }
+
+  def __init__(self, address=0x3E, interruptPin=None, resetPin=None, oscillatorPin=None):
+    self.address = address
+    self.interruptPin = interruptPin
+    self.resetPin = resetPin
+    self.oscillatorPin = oscillatorPin
+  
