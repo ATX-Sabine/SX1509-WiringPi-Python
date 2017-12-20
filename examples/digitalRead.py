9b343397af810d01718c12d8f1e9da5ce6dc31d2
Kas Perch <the@nodebotani.st>
2017-12-20 11:48:50 -0600

    coordinating examples

--- examples/digitalRead.py
+++ examples/digitalRead.py
@@ -3,3 +3,5 @@ import sys
 sys.path.append('../')
 
 from SX1509 import SX1509
+
+sx1509 = SX1509(0x3E)
--- examples/digitalWrite.py
+++ examples/digitalWrite.py
@@ -1,5 +1,6 @@
 import sys
-
-sys.path.append('..')
+sys.path.append('../')
 
 from SX1509 import SX1509
+
+sx1509 = SX1509(0x3E)
