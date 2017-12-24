098b258118c99532577b2bed93fceb58bb535272
Kas Perch <the@nodebotani.st>
2017-12-23 21:05:28 -0600

    IT WORKS!

--- SX1509.py
+++ SX1509.py
@@ -38,7 +38,7 @@ class SX1509:
     data[0] = self.i2c.readReg8(self.device, self.REGISTERS['CHECK'])
     data[1] = self.i2c.readReg8(self.device, self.REGISTERS['CHECK']+1)
     print(data) 
-    
+
   def reset(self, hardware):
     if hardware:
       if self.resetPin == None:
@@ -48,7 +48,88 @@ class SX1509:
         wiringpi.digitalWrite(self.resetPin, 0)
         wiringpi.digitalWrite(self.resetPin, 1)
     else:
-      wiringpi.wiringPiI2CWriteReg8(self.address, self.REGISTERS['RESET'], 0x12)
-      wiringpi.wiringPiI2CWriteReg8(self.address, self.REGISTERS['RESET'], 0x34)
+      self.i2c.writeReg8(self.device, self.REGISTERS['RESET'], 0x12)
+      self.i2c.writeReg8(self.device, self.REGISTERS['RESET'], 0x34)
 
-  
+  def startInternalClock(self): 
+    clockStatus = self.i2c.readReg8(self.device, self.REGISTERS['CLOCK'])
+    clockStatus = [0x00, clockStatus]
+    miscStatus = self.i2c.readReg8(self.device, self.REGISTERS['MISC'])
+    miscStatus = [0x00, miscStatus]
+    clockStatus = self.useBitMask(clockStatus, 5, False)
+    clockStatus = self.useBitMask(clockStatus, 6, True)
+    miscStatus = self.useBitMask(miscStatus, 6, False)
+    miscStatus = self.useBitMask(miscStatus, 5, False)
+    miscStatus = self.useBitMask(miscStatus, 4, True)
+    print(clockStatus)
+    print(miscStatus)
+    self.i2c.writeReg8(self.device, self.REGISTERS['MISC'], miscStatus[1])
+    self.i2c.writeReg8(self.device, self.REGISTERS['CLOCK'], clockStatus[1])
+
+  def setDisableInputBuffer(self, pin, disableInputBuffer):
+    disableInputBufferStatus = [0x00, 0x00]
+    disableInputBufferStatus[0] = self.i2c.readReg8(self.device, self.REGISTERS['DISABLE_INPUT_BUFFER'])
+    disableInputBufferStatus[1] = self.i2c.readReg8(self.device, self.REGISTERS['DISABLE_INPUT_BUFFER'] + 1)
+    disableInputBufferStatus = self.useBitMask(disableInputBufferStatus, pin, disableInputBuffer)
+    self.i2c.writeReg8(self.device, self.REGISTERS['DISABLE_INPUT_BUFFER'], disableInputBufferStatus[0])
+    self.i2c.writeReg8(self.device, self.REGISTERS['DISABLE_INPUT_BUFFER'] + 1, disableInputBufferStatus[1])
+
+  def setPullupResistor(self, pin, pullupResistorOn):
+    pullupResistorStatus = [0x00, 0x00]
+    pullupResistorStatus[0] = self.i2c.readReg8(self.device, self.REGISTERS['PULLUP_RESISTORS'])
+    pullupResistorStatus[1] = self.i2c.readReg8(self.device, self.REGISTERS['PULLUP_RESISTORS'] + 1)
+    pullupResistorStatus = self.useBitMask(pullupResistorStatus, pin, pullupResistorOn)
+    self.i2c.writeReg8(self.device, self.REGISTERS['PULLUP_RESISTORS'], pullupResistorStatus[0])
+    self.i2c.writeReg8(self.device, self.REGISTERS['PULLUP_RESISTORS'] + 1, pullupResistorStatus[1])
+
+  def setPinDirection(self, pin, direction):
+    currentPinState = [0x00, 0x00]
+    currentPinState[0] = self.i2c.readReg8(self.device, self.REGISTERS['PIN_DIRECTION'])
+    currentPinState[1] = self.i2c.readReg8(self.device, self.REGISTERS['PIN_DIRECTION'] + 1)
+    bitOn = True
+    if direction == 'output':
+      bitOn = False
+    newPinState = self.useBitMask(currentPinState, pin, bitOn)
+    self.i2c.writeReg8(self.device, self.REGISTERS['PIN_DIRECTION'], newPinState[0])
+    self.i2c.writeReg8(self.device, self.REGISTERS['PIN_DIRECTION'] + 1, newPinState[1])
+
+  def setDigitalPinValue(self, pin, value):
+    currentPinState = [0x00, 0x00]
+    currentPinState[0] = self.i2c.readReg8(self.device, self.REGISTERS['PIN_DATA'])
+    currentPinState[1] = self.i2c.readReg8(self.device, self.REGISTERS['PIN_DATA'] + 1)
+    bitOn = True
+    if not bitOn == 1:
+      bitOn = False
+    newPinState = self.useBitMask(currentPinState, pin, bitOn)
+    self.i2c.writeReg8(self.device, self.REGISTERS['PIN_DATA'], newPinState[0])
+    self.i2c.writeReg8(self.device, self.REGISTERS['PIN_DATA'] + 1, newPinState[1])
+
+  def enableLEDDriver(self, pin, LEDDriverOn):
+    ledDriverState = [0x00, 0x00]
+    ledDriverState[0] = self.i2c.readReg8(self.device, self.REGISTERS['LED_DRIVER'])
+    ledDriverState[1] = self.i2c.readReg8(self.device, self.REGISTERS['LED_DRIVER'] + 1)
+    ledDriverState = self.useBitMask(ledDriverState, pin, LEDDriverOn)
+    self.i2c.writeReg8(self.device, self.REGISTERS['LED_DRIVER'], ledDriverState[0])
+    self.i2c.writeReg8(self.device, self.REGISTERS['LED_DRIVER'] + 1, ledDriverState[1])
+
+  def setPWMPinValue(self, pin, value):
+    # byteValue = 0xFF & value
+    self.i2c.writeReg8(self.device, self.REGISTERS['PWM_INTENSITY'][pin - 1], value)
+
+  def useBitMask(self, currentState, bit, bitOn):
+    mask = [0x00, 0x00]
+    maskBase = 0x0000
+    maskBase |= (1 << bit)
+    if not bitOn:
+      maskBase = ~maskBase
+    highByte = maskBase >> 8
+    if bitOn:
+      mask[0] = currentState[0] | highByte
+      mask[1] = currentState[1] | maskBase
+    else:
+      mask[0] = currentState[0] & highByte
+      mask[1] = currentState[1] & maskBase
+    return mask
+
+
+
---
+++ examples/PWM.py
@@ -0,0 +1,29 @@
+import time
+import sys
+sys.path.append('../')
+
+from SX1509 import SX1509
+
+expander = SX1509(0x3E)
+
+expander.startInternalClock()
+expander.setDisableInputBuffer(5, True)
+expander.setDisableInputBuffer(6, True)
+expander.setDisableInputBuffer(7, True)
+expander.setPullupResistor(5, False)
+expander.setPullupResistor(6, False)
+expander.setPullupResistor(7, False)
+expander.setPinDirection(5, 'output')
+expander.setPinDirection(6, 'output')
+expander.setPinDirection(7, 'output')
+expander.setDigitalPinValue(5, 0)
+expander.setDigitalPinValue(6, 0)
+expander.setDigitalPinValue(7, 0)
+expander.startInternalClock()
+expander.enableLEDDriver(5, True)
+expander.enableLEDDriver(6, True)
+expander.enableLEDDriver(7, True)
+
+expander.setPWMPinValue(5, 0xFF)
+expander.setPWMPinValue(6, 0x00)
+expander.setPWMPinValue(7, 0xFF)
